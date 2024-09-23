605bce949bcf06413723fe105a96af52dfd6bd99
Claudia Lainfiesta <95670577+Claudia-Lainfiesta@users.noreply.github.com>
2024-09-23 01:47:42 +0000

    -

--- Main.java
+++ Main.java
@@ -3,13 +3,10 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws Exception{
-        try {
-            BmpHandlerCore handler = new BmpHandlerCore("imagenesPrueba/image3.bmp");
-            handler.RedImage("imagenesPrueba/Image-red.bmp");
-            handler.GreenImage("imagenesPrueba/Image-green.bmp");
-            handler.BlueImage("imagenesPrueba/Image-blue.bmp");
-        } catch (IOException e) {
-            System.out.println("ERROR");
-        }
+        BmpHandlerCore handler = new BmpHandlerCore("imagenesPrueba/image3.bmp");
+        handler.RedImage("imagenesPrueba/Image-red.bmp");
+        handler.GreenImage("imagenesPrueba/Image-green.bmp");
+        handler.BlueImage("imagenesPrueba/Image-blue.bmp");
+        System.out.println("ERROR");
     }
 }
